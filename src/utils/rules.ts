e74a5bddcf58c940f65e14b88df5f78dac8bceec
Son Pham <78460399+sonpt-afk@users.noreply.github.com>
2024-07-29 20:54:30 +0700

    confirm pass validate

--- src/utils/rules.ts
+++ src/utils/rules.ts
@@ -1,12 +1,56 @@
-export const rules = {
+import type { RegisterOptions, UseFormGetValues } from 'react-hook-form'
+
+type Rules = { [key in 'email' | 'password' | 'confirm_password']?: RegisterOptions }
+
+export const getRules = (getValues?: UseFormGetValues<any>): Rules => ({
   email: {
     required: {
       value: true,
-      message: 'Email is required'
+      message: 'Email là bắt buộc'
     },
     pattern: {
-      value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-      message: 'Email is invalid'
+      value: /^\S+@\S+\.\S+$/,
+      message: 'Email không đúng định dạng'
+    },
+    maxLength: {
+      value: 160,
+      message: 'Độ dài từ 5 - 160 ký tự'
+    },
+    minLength: {
+      value: 5,
+      message: 'Độ dài từ 5 - 160 ký tự'
     }
+  },
+  password: {
+    required: {
+      value: true,
+      message: 'Password là bắt buộc'
+    },
+    maxLength: {
+      value: 160,
+      message: 'Độ dài từ 6 - 160 ký tự'
+    },
+    minLength: {
+      value: 6,
+      message: 'Độ dài từ 6 - 160 ký tự'
+    }
+  },
+  confirm_password: {
+    required: {
+      value: true,
+      message: 'Nhập lại password là bắt buộc'
+    },
+    maxLength: {
+      value: 160,
+      message: 'Độ dài từ 6 - 160 ký tự'
+    },
+    minLength: {
+      value: 6,
+      message: 'Độ dài từ 6 - 160 ký tự'
+    },
+    validate:
+      typeof getValues === 'function'
+        ? (value) => value === getValues('password') || 'Nhập lại password không khớp'
+        : undefined
   }
-}
+})
