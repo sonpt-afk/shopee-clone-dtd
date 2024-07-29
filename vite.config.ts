a0e8b3c568ad6eea08eb32c1e4114fda05a9dcb9
Son Pham <78460399+sonpt-afk@users.noreply.github.com>
2024-07-29 13:05:37 +0700

    Enable sourcemap css cho Vite & Fix import path

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
-// eslint-disable-next-line import/no-unresolved
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), visualizer()] as any,
-  test: {
-    environment: 'jsdom',
-    setupFiles: path.resolve(__dirname, './vitest.setup.js')
-  },
+  plugins: [react()],
   server: {
     port: 3000
   },
