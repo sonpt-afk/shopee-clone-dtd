693886148ffea09edc8d34a1bff866320bfed763
Son Pham <78460399+sonpt-afk@users.noreply.github.com>
2024-07-30 14:01:07 +0700

    interceptor

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import useRouteElements from './useRouteElements'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   const routeElements = useRouteElements()
-  return <div>{routeElements}</div>
+  return (
+    <div>
+      {routeElements}
+      <ToastContainer />
+    </div>
+  )
 }
 
 export default App
--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,15 +1,34 @@
-import axios, { type AxiosInstance } from 'axios'
+import axios, { AxiosError, type AxiosInstance } from 'axios'
+import HttpStatusCode from 'src/constants/httpStatusCode.enum'
+import { isAxiosUnprocessableEntityError } from './utils'
+import { ToastContainer, toast } from 'react-toastify'
 
 class Http {
   instance: AxiosInstance
   constructor() {
     this.instance = axios.create({
       baseURL: 'https://api-ecom.duthanhduoc.com/',
-      timeout: 1000,
       headers: {
         'Content-Type': 'application/json'
       }
     })
+
+    // Add a response interceptor
+    this.instance.interceptors.response.use(
+      function (response) {
+        // Any status code that lie within the range of 2xx cause this function to trigger
+        // Do something with response data
+        return response
+      },
+      function (error: AxiosError) {
+        if (error.response?.status !== HttpStatusCode.UnprocessableEntity) {
+          const data: any | undefined = error.response?.data
+          const message = data?.message || error.message
+          toast.error(message)
+        }
+        return Promise.reject(error)
+      }
+    )
   }
 }
 
