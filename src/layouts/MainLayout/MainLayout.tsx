00ad87b3e0fe5180693f11802152fde24101e83a
Son Pham <78460399+sonpt-afk@users.noreply.github.com>
2024-07-30 17:34:07 +0700

    made Header

---
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from 'src/components/Header'
+import Footer from 'src/components/Footer'
+
+interface Props {
+  children?: React.ReactNode
+}
+export default function MainLayout({ children }: Props) {
+  return (
+    <div>
+      <Header />
+      {children}
+      <Footer></Footer>
+    </div>
+  )
+}
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,43 +1,79 @@
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { schema, Schema } from 'src/utils/rules'
+import { useMutation } from '@tanstack/react-query'
+import { omit } from 'lodash'
+import { isAxiosUnprocessableEntityError } from 'src/utils/utils'
+import { ResponseApi } from 'src/types/utils.type'
+import { loginAccount } from 'src/apis/auth.api'
+import Input from 'src/components/Input'
+type FormData = Omit<Schema, 'confirm_password'>
 
+const loginSchema = schema.omit(['confirm_password'])
 export default function Login() {
   const {
+    setError,
     register,
     handleSubmit,
     formState: { errors }
-  } = useForm()
-
-  const onSubmit = handleSubmit((data) => {
-    console.log(data)
+  } = useForm<FormData>({
+    resolver: yupResolver(loginSchema)
   })
 
+  const loginAccountMutation = useMutation({
+    mutationFn: (body: Omit<FormData, 'confirm_password'>) => loginAccount(body)
+  })
+  const onSubmit = handleSubmit((data) => {
+    loginAccountMutation.mutate(data, {
+      onSuccess: (data) => {
+        console.log(data)
+      },
+      onError: (error) => {
+        if (isAxiosUnprocessableEntityError<ResponseApi<FormData>>(error)) {
+          const formError = error.response?.data?.data
+          console.log(formError)
+          if (formError) {
+            Object.keys(formError).forEach((key) => {
+              setError(key as keyof Omit<FormData, 'confirm_password'>, {
+                message: formError[key as keyof Omit<FormData, 'confirm_password'>],
+                type: 'Server'
+              })
+            })
+          }
+        }
+      }
+    })
+  })
+  const registerAccountMutation = useMutation({
+    mutationFn: (body: Omit<FormData, 'confirm_password'>) => loginAccount(body)
+  })
   return (
     <div className='bg-orange'>
       <div className='max-w-7xl mx-auto px-4'>
         <div className='grid grid-cols-1 lg:grid-cols-5 py-12 lg:py-32 lg:pr-10'>
           <div className='lg:col-span-2 lg:col-start-4'>
-            <form className='p-10 rounded bg-white shadow-sm' onSubmit={onSubmit}>
+            <form className='p-10 rounded bg-white shadow-sm' onSubmit={onSubmit} noValidate>
               <div className='text-2xl'>Đăng nhập</div>
-              <div className='mt-8'>
-                <input
-                  type='email'
-                  name='email'
-                  className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
-                  placeholder='Email'
-                />
-                <div className='mt-1 text-red-600 min-h-[1rem] text-sm'></div>
-              </div>
-              <div className='mt-3'>
-                <input
-                  type='password'
-                  name='password'
-                  autoComplete='on'
-                  className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
-                  placeholder='Password'
-                />
-                <div className='mt-1 text-red-600 min-h-[1rem] text-sm'></div>
-              </div>
+              <Input
+                type='email'
+                name='email'
+                register={register}
+                errorMessages={errors.email?.message}
+                placeholder='Email'
+                className='mt-8'
+                autoComplete='on'
+              />
+
+              <Input
+                type='password'
+                name='password'
+                register={register}
+                errorMessages={errors.password?.message}
+                placeholder='Password'
+                autoComplete='on'
+                className='mt-3'
+              />
               <div className='mt-3'>
                 <button
                   type='submit'
